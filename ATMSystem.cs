6500472b18348386853e556447292174c81a4f5d
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-03-05 23:47:42 +0200

    Initial commit

---
+++ ATMSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATMSystem
+{
+    class ATMSystem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ATM System");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+<h2 align="center">CSharp-ATM-System-Console</h2>
